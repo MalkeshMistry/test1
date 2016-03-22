5ac6284b8dbc05174d8026e87562fa7124340d05
Malkesh <malkesh.mistry.comp@gmail.com>
2016-03-22 15:23:51 +0530

    test

--- StudentCRUD/Models/Address.cs
+++ StudentCRUD/Models/Address.cs
@@ -7,7 +7,6 @@ namespace StudentCRUD.Models
 {
     public class Address
     {
-        // Test
         public int ID { get; set; }
 	    public string StreetAddress { get; set; }
 	    public string City { get; set; }
